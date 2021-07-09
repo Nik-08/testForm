f890c438a3cfa3b129fce21a42b66d085fb42ffe
Nik-08 <69086824+Nik-08@users.noreply.github.com>
2021-07-09 18:01:42 +0700

    beta-version

--- index.js
+++ index.js
@@ -3,22 +3,87 @@
 const elemAdd = document.querySelector(".elem__head_add"),
   formProduct = document.querySelector(".form__product"),
   formContent = document.querySelector(".form__content"),
+  form = document.querySelector(".form"),
   formBtn = document.querySelector(".form__btn"),
   formFooter = document.querySelector(".form__footer"),
   elemItem = document.querySelectorAll(".elem__item"),
   btnProduct = document.querySelector(".btn-product"),
-  elemProduct = document.querySelector(".elem__product"),
+  btnPay = document.querySelector(".btn-pay"),
+  elemProducts = document.querySelector(".elem__products"),
+  btnText = document.querySelector(".btn__text"),
+  loader = document.querySelector(".loader"),
+  successfulForm = document.querySelector(".successful__form"),
+  failedForm = document.querySelector(".failed__form"),
   elemItems = document.querySelector(".elem__items");
+let url = window.location.href;
 
-// const passElem = () => {
-// 	parentDiv.insertBefore(newElemProduct, elemProduct);
-// }
-let arr = [];
+let arr = [],
+  data = {};
 
 const sendForm = () => {
-  formBtn.addEventListener("click", (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
+    loader.style.display = "block";
+    btnText.style.display = "none";
+
+    setTimeout(post, 1500);
+
+    function post() {
+      const formData = new FormData(form); // создали объект который считывает с формы
+      let body = {}; // записываем в объект
+      formData.forEach((val, key) => {
+        // перебираем массив
+        body[key] = val;
+      });
+      postData(body)
+        .then((request) => {
+          if (request.status !== 200) {
+            // проверяем статус
+            throw new Error("status network now 200");
+          }
+          successfulForm.style.display = "block";
+          formContent.style.display = "none";
+          formFooter.style.display = "none";
+          formBtn.style.display = "none";
+          btnPay.style.display = "block";
+          window.history.pushState(
+            "object or string",
+            "Title",
+            url + "&paymentsuccess"
+          );
+        })
+        .catch(() => {
+          failedForm.style.display = "block";
+          formContent.style.display = "none";
+          formFooter.style.display = "none";
+          formBtn.style.display = "none";
+          btnPay.style.display = "block";
+          btnPay.textContent = "Try to pay again";
+          btnPay.classList.add("_failed");
+          window.history.pushState(
+            "object or string",
+            "Title",
+            url + "&paymenterror"
+          );
+          console.log(url);
+        })
+        .finally(() => {
+          form.querySelectorAll("input").forEach((elem) => {
+            elem.value = "";
+          });
+        });
+    }
   });
+
+  const postData = (body) => {
+    return fetch("server.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  };
 };
 
 sendForm();
@@ -40,10 +105,10 @@ const removeClass = () => {
   });
 };
 
-// const removeProduct = () => {
-
-// };
-// removeProduct();
+const removeProducts = () => {
+  let par = document.querySelectorAll(".new__elem");
+  par.forEach((e) => e.remove());
+};
 
 const selectProduct = () => {
   elemItem.forEach((e) => {
@@ -51,8 +116,7 @@ const selectProduct = () => {
       let target = event.currentTarget;
       let arr = [];
 
-      let par = document.querySelectorAll(".new__elem");
-      par.forEach((e) => e.remove());
+      removeProducts();
 
       if (target.closest(".elem__item")) {
         removeClass();
@@ -74,7 +138,12 @@ const selectProduct = () => {
         for (let index = 0; index < max; index++) {
           // создание элемента
           let newElemProduct = document.createElement("div");
-          newElemProduct.classList.add("form__elem", "elem", "new__elem");
+          newElemProduct.classList.add(
+            "form__elem",
+            "elem",
+            "new__elem",
+            "elem__product"
+          );
           newElemProduct.innerHTML = `<div class="elem__head">
             <span class="elem__title"
             >Product <span class="elem__numb">${
@@ -90,15 +159,27 @@ const selectProduct = () => {
             <input
             type="text"
             class="elem__input"
-            placeholder="for example, sylicon wine cup"
-          />`;
+              placeholder="for example, sylicon wine cup"
+            />
+            <div class="form__elem elem elem__link">
+            <div class="elem__description">
+              <span class="elem__text">
+                Enter link to the similar product as a reference
+              </span>
+            </div>
+            <input
+              type="text"
+              class="elem__input"
+              placeholder="https://..."
+            />
+          </div>`;
 
           arr.push(newElemProduct);
         }
 
         for (let i = 0; i < arr.length - 1; i++) {
           // вставка на страницу
-          elemProduct.appendChild(arr[i]);
+          elemProducts.appendChild(arr[i]);
         }
         formContent.style.overflowY = "scroll";
       }
@@ -107,6 +188,7 @@ const selectProduct = () => {
 };
 
 const addMore = () => {
+  //добавить больше
   elemAdd.addEventListener("click", (event) => {
     formContent.style.display = "none";
     formFooter.style.display = "none";
@@ -117,8 +199,48 @@ const addMore = () => {
     selectProduct();
   });
 };
-
 addMore();
+
+const amountElements = () => {
+  const elemProduct = document.querySelectorAll(".elem__product"),
+    btnPrice = document.querySelector(".btn__price");
+  let elemNumb = document.querySelectorAll(".elem__numb");
+
+  let array = [];
+  elemProduct.forEach((e) => {
+    array.push(e);
+
+    switch (array.length) {
+      case 2:
+        btnPrice.textContent = "44";
+        break;
+      case 3:
+        btnPrice.textContent = "60";
+        break;
+      case 4:
+        btnPrice.textContent = "72";
+        break;
+      case 5:
+        btnPrice.textContent = "80";
+        break;
+      default:
+        btnPrice.textContent = "24.99";
+    }
+  });
+};
+
+const removeProduct = () => {
+  let cross = document.querySelectorAll("._cross");
+  cross.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      if (item.closest(".new__elem")) {
+        item.closest(".new__elem").remove();
+
+        amountElements();
+      }
+    });
+  });
+};
 
 btnProduct.addEventListener("click", (event) => {
   event.preventDefault();
@@ -128,16 +250,22 @@ btnProduct.addEventListener("click", (event) => {
   formProduct.style.display = "none";
   btnProduct.style.display = "none";
 
-  let cross = document.querySelectorAll("._cross");
+  removeProduct();
+  amountElements();
+});
 
-  cross.forEach((item) => {
-    item.addEventListener("click", (event) => {
-      let target = event.target;
-      console.log(target);
+btnPay.addEventListener("click", (event) => {
+  event.preventDefault();
+  formContent.style.display = "block";
+  formFooter.style.display = "flex";
+  formBtn.style.display = "block";
+  failedForm.style.display = "none";
+  successfulForm.style.display = "none";
+  btnPay.style.display = "none";
+  loader.style.display = "none";
+  btnText.style.display = "block";
+  window.history.pushState("object or string", "Title", url + "");
+  formContent.style.overflowY = "visible";
 
-      if (item.closest(".new__elem")) {
-        item.closest(".new__elem").remove();
-      }
-    });
-  });
+  removeProducts();
 });
